66fd1c8723d61a2286f86d60ff21bdfd151948f1
Antoine Lardant <antoine.lardant@gmail.com>
2023-05-26 21:38:02 +0200

    style header

--- GarageVParrot/wwwroot/js/site.js
+++ GarageVParrot/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*underline active link*/
 
-// Write your JavaScript code.
+document.querySelectorAll(".nav-link").forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+    }
+});
