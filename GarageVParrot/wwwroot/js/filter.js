eb177c665bed88a0e96399afb9117360a2256f91
Antoine Lardant <antoine.lardant@gmail.com>
2023-06-27 16:42:56 +0200

    move the filter script to a filter.js

--- GarageVParrot/wwwroot/js/filter.js
+++ GarageVParrot/wwwroot/js/filter.js
@@ -73,13 +73,106 @@ function hideFilter(filterName) {
     filterUpArrow.style.display = "none";
 }
 
-
-
 window.addEventListener("resize", handleFilterBoxVisibility);
 window.addEventListener("DOMContentLoaded", handleFilterBoxVisibility);
 
 
+
+//handle the input change event
+$('input').on('change', function () {
+    var selectedBrands = $('input[type="checkbox"][name="car-brand"]:checked').map(function () {
+        return $(this).val();
+    }).get();
+        var selectedCategories = $('input[type="checkbox"][name="car-category"]:checked').map(function () {
+        return $(this).val();
+    }).get();
+        var selectedModels = $('input[type="checkbox"][name="car-model"]:checked').map(function () {
+        return $(this).val();
+    }).get();
+        var selectedEnergies = $('input[type="checkbox"][name="car-energy"]:checked').map(function () {
+        return $(this).val();
+    }).get();
+        var selectedGears = $('input[type="checkbox"][name="car-gear"]:checked').map(function () {
+        return $(this).val();
+    }).get();
+        var selectedCritair = $('input[type="checkbox"][name="car-critair"]:checked').map(function () {
+        return $(this).val();
+    }).get();
+        var hasAircon = $('input[type="checkbox"][name="AirConditionner"]:checked').map(function () {
+        return $(this).val();
+    }).get()
+        var hasBt = $('input[type="checkbox"][name="Bluetooth"]:checked').map(function () {
+        return $(this).val();
+    }).get()
+        var hasRevRad = $('input[type="checkbox"][name="ReversingRadar"]:checked').map(function () {
+        return $(this).val();
+    }).get()
+        var hasGps = $('input[type="checkbox"][name="Gps"]:checked').map(function () {
+        return $(this).val();
+    }).get()
+        var hasSpeedReg = $('input[type="checkbox"][name="SpeedRegulator"]:checked').map(function () {
+        return $(this).val();
+    }).get()
+        var hasAbs = $('input[type="checkbox"][name="Abs"]:checked').map(function () {
+        return $(this).val();
+    }).get()
+        var minYear = $('input[type="text"][name="car-minyear"]').map(function () {
+        return $(this).val();
+    }).get()
+        var maxYear = $('input[type="text"][name="car-maxyear"]').map(function () {
+        return $(this).val();
+    }).get()
+        var minPrice = $('input[type="text"][name="car-minprice"]').map(function () {
+        return $(this).val();
+    }).get()
+        var maxPrice = $('input[type="text"][name="car-maxprice"]').map(function () {
+        return $(this).val();
+    }).get()
+        var minKm = $('input[type="text"][name="car-minkm"]').map(function () {
+        return $(this).val();
+    }).get()
+        var maxKm = $('input[type="text"][name="car-maxkm"]').map(function () {
+        return $(this).val();
+    }).get()
+
+    //send AJAX request to retrieve filtered product data
+    $.ajax({
+        url: '/Cars/GetFilteredCars',
+        type: 'POST',
+        dataType: 'html',
+        data: {
+            brandList: selectedBrands,
+            categoryList: selectedCategories,
+            modelList: selectedModels,
+            energyList: selectedEnergies,
+            gearList: selectedGears,
+            critairList: selectedCritair,
+            hasAirConditionner: hasAircon,
+            hasBluetooth: hasBt,
+            hasReversingRadar: hasRevRad,
+            hasGPS: hasGps,
+            hasSpeedRegulator: hasSpeedReg,
+            hasABS: hasAbs,
+            MinYear: minYear,
+            MaxYear: maxYear,
+            MinPrice: minPrice,
+            MaxPrice: maxPrice,
+            MinKm: minKm,
+            MaxKm: maxKm
+        },
+        success: function (result) {
+            $('.cars-cards').html(result);
+        },
+        error: function () {
+            console.log('An error occured');
+        }
+    });
+});
+
+
+// Display error message if input fields are not int 
 $('input[type="text"]').keyup(function () {
+    // get the fields
     var minYearInput = document.getElementById("minYear");
     var maxYearInput = document.getElementById("maxYear");
     var yearError = document.getElementById("year-error");
@@ -91,7 +184,7 @@ $('input[type="text"]').keyup(function () {
     var kmError = document.getElementById("km-error");
     var regex = /^[0-9]+$/;
 
-
+    // if input has a value and it's not an int, display error message' 
     if (minYearInput.value && !minYearInput.value.match(regex) || maxYearInput.value && !maxYearInput.value.match(regex)) {
         yearError.style.display = "block";
         // prevent the form to be sent
@@ -113,6 +206,5 @@ $('input[type="text"]').keyup(function () {
     } else {
         kmError.style.display = "none";
     }
-
     return true
 })
